282e72a4e456656b0667d4b03b0d9c21f9dff666
briannamonsegue <bmonsegue2020@brooklynprephs.org>
2019-11-07 23:22:29 +0000

    work in progress

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+$("button").click(function
+(){
+let input= $("input").val();
+console.log(input)
+$(".output").text(input)
+});
 
 
 
-  
-  
-  
-  
+
+
+
 });
